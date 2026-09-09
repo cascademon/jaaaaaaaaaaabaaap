832e7c868649176e17ea3a60663256b8e71ab25b
자바바바밥 <104069100+cascademon@users.noreply.github.com>
2026-09-09 22:48:12 +0900

    [Passed] 길이 단위 변환하기 (97ms, 10MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 30.48;
+        int b = 160934;
+        System.out.printf("9.2ft = %.1fcm\n1.3mi = %.1fcm", (a*9.2),(b*1.3));
+    }
+}
